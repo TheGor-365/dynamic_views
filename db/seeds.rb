733ff0a8000b02b2995d8038976957b77bd72e6c
Gor <7495500@gmail.com>
2025-07-26 14:37:47 +0300

    Add product model and products to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,5 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Product.create!([
+  { name: "iPhone X", description: "Смартфон Apple iPhone X, 64GB", price: 29990 },
+  { name: "MacBook Air", description: "Лёгкий ноутбук Apple MacBook Air 2020", price: 79990 },
+  { name: "iPad Pro", description: "Планшет Apple iPad Pro 11-inch", price: 59990 }
+])
