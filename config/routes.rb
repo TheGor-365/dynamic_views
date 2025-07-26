07f6ef7a95dd88a932c7c87c297f657bdcfec62a
Gor <7495500@gmail.com>
2025-07-26 14:53:41 +0300

    Restore git files

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root "products#index"
+  resources :products, only: [:index, :show]
+  
+  get "up" => "rails/health#show", as: :rails_health_check
+end
---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+Product.create!([
+  { name: "iPhone X", description: "Смартфон Apple iPhone X, 64GB", price: 29990 },
+  { name: "MacBook Air", description: "Лёгкий ноутбук Apple MacBook Air 2020", price: 79990 },
+  { name: "iPad Pro", description: "Планшет Apple iPad Pro 11-inch", price: 59990 }
+])
