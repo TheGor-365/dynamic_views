f25446523b85c3ab8a4ec015576e493e633d3aba
Gor <7495500@gmail.com>
2025-07-26 15:14:20 +0300

    Modify application layout page, add controller content for actions, add products/index.html.erb base content

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,10 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
   end
 
   def show
+    @product = Product.find(params[:id])
+    render partial: "products/product_details", locals: { product: @product }
   end
 end
