577b7e18f259c4de88b2864a9a5d10d6cdfbf0c3
Gor <7495500@gmail.com>
2025-07-26 14:53:41 +0300

    Restore git files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,4 +8,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+=======
+  root "products#index"
+  resources :products, only: [:index, :show]
+  
+  get "up" => "rails/health#show", as: :rails_health_check
+>>>>>>> Restore git files
 end
